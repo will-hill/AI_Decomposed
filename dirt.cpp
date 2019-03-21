294b836e2b50a6d8063137658d126b1eb51f902d
Will Hill <william.c.hill@gmail.com>
2019-03-21 00:11:00 -0400

    adsfasdfaf

--- dirt.cpp
+++ dirt.cpp
@@ -17,6 +17,25 @@ using namespace std;
 using namespace cv;
 
 extern "C"
+int *max_2x2_kernel(const int *mat, const int width, const int height) {
+
+    int curr_ttl = width * height;
+    vector<int> shrunk_vec;
+    for (int i = 0; i < curr_ttl; i++) {
+        if (i >= (width * (height - 1))) {
+            break;
+        }
+        if (i != 0 and (i + 1) % width == 0) {
+            continue;
+        }
+        vector<int> window{mat[i], mat[(i + 1)], mat[(i + width)], mat[(i + 1 + width)]};
+        shrunk_vec.push_back(*std::max_element(window.begin(), window.end()));
+    }
+
+    return &shrunk_vec[0];
+}
+
+extern "C"
 int *reverse_array(const int *input_array, const int size) {
     int *rev_array = new int[size];
     for (int i = 0; i < size; i++) {
@@ -36,14 +55,6 @@ int *get_data(char *image_file_path) {
         pixel_ints[i] = pixel;
     }
     return pixel_ints;
-}
-
-extern "C"
-int *apply_kernel(const int *image, const int image_width) {
-
-
-    int *convolution= new int[total_size];
-    return convolution;
 }
 
 int main(int argc, char *argv[]) {
