2bdbbfc564845b1b114d3cc23e8136900bc34aa3
will-hill <william.c.hill@gmail.com>
2019-03-25 17:55:10 -0400

    adding performance testing for C++ and Python

--- cpp/convolution.cpp
+++ cpp/convolution.cpp
@@ -3,8 +3,9 @@
 #include <algorithm>
 #include <numeric>
 #include <tgmath.h>
+#include <array>
 
-extern "C" int *max_2x2_kernel(const int *pixels, const int width, const int height) {
+std::vector<int> pool_2x2_kernel(std::vector<int> pixels, const int width, const int height, const int funct) {
     // total pixels to convolve
     int total_pixels = width * height;
     // to hold convolution
@@ -18,35 +19,59 @@ extern "C" int *max_2x2_kernel(const int *pixels, const int width, const int hei
         if (i != 0 and (i + 1) % width == 0) {
             continue;
         }
-        // grab 2x2 window, calculate mean and add to convolution
+        // grab 2x2 window, calculate max or mean and add to convolution
         std::vector<int> window{pixels[i], pixels[(i + 1)], pixels[(i + width)], pixels[(i + 1 + width)]};
-        double sum = std::accumulate(window.begin(), window.end(), 0.0);
-        int mean = (int) std::round(sum / window.size());
-        convolution.push_back(mean);
-    }
-    // return as array pointer
-    return &convolution[0];
-}
 
-extern "C" int *recurse_convolution(const int *pixels, const int width, const int height, const int cnt) {
-    std::cout << "start\n";
-    int w = width, h = height;
-    int *convolution = new int[h * w];
-    for (int i = 0; i < w * h; i++) {
-        convolution[i] = pixels[i];
-    }
-    for (int i = 0; i < cnt; i++) {
-        convolution = max_2x2_kernel(convolution, width, height);
-        w--;
-        h--;
+        double y = 255;
+        if (funct == 0) {
+            double sum = std::accumulate(window.begin(), window.end(), 0.0);
+            y = sum / window.size();
+        } else {
+            y = *max_element(window.begin(), window.end());
+        }
+        convolution.push_back(std::nearbyint(y));
     }
     return convolution;
 }
 
+extern "C" int *
+recurse_convolution(const int *pixels, const int width, const int height, const int recurse_cnt, const int use_max) {
+    int w = width, h = height, pixel_count = h * w;
+    std::vector<int> convolution(pixel_count);
+    for (int i = 0; i < w * h; i++) {
+        convolution[i] = pixels[i];
+    }
+    for (int i = 0; i < recurse_cnt; i++) {
+        convolution = pool_2x2_kernel(convolution, w, h, use_max);
+        w--;
+        h--;
+    }
+
+    int *convo_array = new int[convolution.size()];
+    std::copy(convolution.begin(), convolution.end(), convo_array);
+    return convo_array;
+}
+
 int main(int argc, char *argv[]) {
     (void) argc;
-    int p[] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
-    recurse_convolution(p, 3, 3, 1000000);
+
+    int mat[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
+    int width = 4;
+    int recurse_cnt = 2;
+
+    int mat_size = sizeof(mat) / sizeof(mat[0]);
+    int height = mat_size / width;
+    int use_max = 0;
+
+    int *output = recurse_convolution(mat, width, height, recurse_cnt, use_max);
+
+    int ret_w = width - recurse_cnt;
+    int ret_h = height - recurse_cnt;
+    int array_size = ret_h * ret_w;
+    std::cout << "---" << std::endl;
+    for (int i = 0; i < array_size; i++) {
+        std::cout << output[i] << " ";
+    }
     //max_2x2_kernel(asdf)
     return 0;
 }
