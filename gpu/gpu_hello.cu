274acf7a0880d2e435431b152806293872f24873
will-hill <william.c.hill@gmail.com>
2019-03-26 11:47:28 -0400

    adding better GPU code

--- gpu/gpu_hello.cu
+++ gpu/gpu_hello.cu
@@ -5,30 +5,48 @@
 #include <algorithm>
 
 __global__ void pooling(int *pixels, int *convolution, int width, int height, int use_max) {
-    int i = threadIdx.x;
 
-    if ((i != 0) && ((i + 1) % width == 0)) {
-        return; // too far to right
-    }
+    int convolution_idx = blockIdx.x * blockDim.x + threadIdx.x;
+    int top_left = (blockIdx.x * width) + (threadIdx.x);
+    int top_right = top_left + 1;
+    int bot_left = top_left + width;
+    int bot_right = bot_left + 1;
 
     // grab 2x2 window, calculate max or mean and add to convolution
-    int *window = new int[4]{pixels[i], pixels[(i + 1)], pixels[(i + width)], pixels[(i + 1 + width)]};
+    int *window = new int[4]{
+            pixels[top_left], pixels[top_right],
+            pixels[bot_left], pixels[bot_right]
+    };
+
     if (use_max == 0) { // use mean
+        double mean = (pixels[top_left] + pixels[top_right] + pixels[bot_left] + pixels[bot_right]) / 4.0;
+        int mean_int = __double2int_rn(mean);
 
-        convolution[i] = (pixels[i] + pixels[(i + 1)] + pixels[(i + width)] + pixels[(i + 1 + width)]) / 4;
+        /*printf("%d: window: %d  %d  %d  %d        %d %d %d %d : %f : %d \n",
+               convolution_idx, top_left, top_right, bot_left, bot_right,
 
+               pixels[top_left], pixels[top_right],
+               pixels[bot_left], pixels[bot_right],
+
+               mean,
+               mean_int);*/
+
+
+        convolution[convolution_idx] = mean_int;
     } else {
 
         int max = -9999;
-
         for (int idx = 0; idx < 4; idx++) {
             if (window[idx] > max) {
                 max = window[idx];
             }
         }
-
-        convolution[i] = max;
+        convolution[convolution_idx] = max;
     }
+}
+
+extern "C" int test(int *pixels) {
+    return -999;
 }
 
 extern "C" int *pooling(int *pixels, const int width, const int height, const int use_max) {
@@ -47,7 +65,7 @@ extern "C" int *pooling(int *pixels, const int width, const int height, const in
     cudaMemcpy(d_pixels, pixels, SIZE * sizeof(int), cudaMemcpyHostToDevice);
 
     // no need to calculate last row
-    pooling << < 1, (SIZE - height - 1) >> > (d_pixels, d_convolution, width, height, use_max);
+    pooling << < (height - 1), (width - 1) >> > (d_pixels, d_convolution, width, height, use_max);
 
     cudaDeviceSynchronize();
 
@@ -59,17 +77,18 @@ extern "C" int *pooling(int *pixels, const int width, const int height, const in
     return convolution;
 }
 
+//  nvcc --ptxas-options=-v --compiler-options '-fPIC' -o gpu.so --shared  gpu_hello.cu
 int main() {
     const int width = 4;
     const int height = 4;
     const int use_max = 0;
 
-    int *pixels = new int[16]{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 12, 13, 14, 15};
+    int *pixels = new int[16]{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
 
     int *convolution = pooling(pixels, width, height, use_max);
 
-    for (int i = 0 ; i < 9 ; i++){
-        std::cout << i << " : " << pixels[i] << "," << pixels[(i + 1)] << "," <<  pixels[(i + width)] << "," <<  pixels[(i + 1 + width)] << " --> " << convolution[i] << " \n";
+    for (int i = 0; i < 9; i++) {
+        std::cout << i << " : " << convolution[i] << " \n";
     }
     return 0;
 }
